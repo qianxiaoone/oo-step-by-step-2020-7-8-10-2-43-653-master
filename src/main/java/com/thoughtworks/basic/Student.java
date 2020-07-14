4500216f03560407869f655643e955998b421c9a
306093226wanggitgitgitgit <306093226@qq.com>
2020-07-14 20:47:08 +0800

    完成需求3

--- src/main/java/com/thoughtworks/basic/Student.java
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -1,19 +1,19 @@
 package com.thoughtworks.basic;
 
 public class Student extends Person {
-    private String classNumber;
+    private Object classNumber;
 
-    public Student(String classNumber) {
+    public Student(Object classNumber) {
         this.classNumber = classNumber;
     }
 
-    public Student(String name, String age, String classNumber) {
+    public Student(String name, String age, Object classNumber) {
         super(name, age);
         this.classNumber = classNumber;
     }
 
     @Override
     public String introduce() {
-        return String.format("%s I am a Student of Class %s.", super.introduce(), this.classNumber);
+        return String.format("%s I am a Student of Class %s.", super.introduce(), this.classNumber.toString());
     }
 }
